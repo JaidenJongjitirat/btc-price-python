6953cb1a470219b3da19791912e6f4e447fdc3a9
Jaiden Jongjitirat <37761702+JaidenJongjitirat@users.noreply.github.com>
2019-07-18 10:32:19 -0700

    Update btc_price_ticker.py

--- btc_price_ticker.py
+++ btc_price_ticker.py
@@ -3,6 +3,7 @@ import time
 import sys
 import math
 
+#fancy colors
 BLUE, RED, WHITE, YELLOW, MAGENTA, GREEN, END = '\33[94m', '\033[91m', '\33[97m', '\33[93m', '\033[1;35m', '\033[1;32m', '\033[0m'
 class start(object):
     #USD Bitcoin price
